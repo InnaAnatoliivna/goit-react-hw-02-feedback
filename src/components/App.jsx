470c66e819ea8431bc6ef8a4dadbb1742cc4867a
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-07-04 22:59:59 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <h1 style={{ color: 'red' }}>React homework template</h1>
     </div>
   );
 };
