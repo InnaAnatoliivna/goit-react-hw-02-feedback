244451b1723c9a37b4207a287af22651607e1f1c
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-07-06 23:39:08 +0200

    +

--- src/components/feedback/feedback.jsx
+++ src/components/feedback/feedback.jsx
@@ -1,11 +1,12 @@
 import { CiFaceFrown, CiFaceMeh, CiFaceSmile } from 'react-icons/ci';
+import { Button } from 'components/feedback/butons-style'
 
 const FeedbackOptions = ({ fbTitle, onLeaveFeedback, option }) => {
     return (
         <div>
-            <button type="button" onClick={onLeaveFeedback}><CiFaceSmile />Good</button>
-            <button type="button" onClick={onLeaveFeedback}><CiFaceMeh />Neutral</button>
-            <button type="button" onClick={onLeaveFeedback}><CiFaceFrown />Bad</button>
+            <Button type="button" onClick={onLeaveFeedback}><CiFaceSmile />Good</Button>
+            <Button type="button" onClick={onLeaveFeedback}><CiFaceMeh />Neutral</Button>
+            <Button type="button" onClick={onLeaveFeedback}><CiFaceFrown />Bad</Button>
         </div>
     )
 }
